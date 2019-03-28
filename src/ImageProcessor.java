e0d48a5b1a0a8c7bd0d38ba793c66f30a688a4e5
Patrick Seute <patrick.seute@radiantsolutions.com>
2019-03-28 12:57:01 -0400

    Add get darkness array

--- src/ImageProcessor.java
+++ src/ImageProcessor.java
@@ -12,4 +12,17 @@ class ImageProcessor {
             e.printStackTrace();
         }
     }
+
+    double[][] getDarknessArray(){
+        double[][] pixels = new double[img.getHeight()][img.getWidth()];
+        for(int i = 0; i < img.getWidth() - 1; i++){
+            for(int j = 0; j < img.getHeight() - 1; j++){
+                int pixel_rgb = img.getRGB(i, j);
+                ColorBright myColor = new ColorBright(pixel_rgb);
+                pixels[i][j] = myColor.getBrightness();
+            }
+        }
+
+        return pixels;
+    }
 }
