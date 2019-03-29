d3cc58d8be1f1f8db70ba4f8b7ea026c576c6e8d
Patrick Seute <patrick.seute@radiantsolutions.com>
2019-03-29 11:31:19 -0400

    Update main to accept input

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
-        ImageProcessor ip = new ImageProcessor("B:\\Pictures\\Saved Pictures\\think.png");
-        double[][] darknessArray = ip.getDarknessArray();
-        ArtGenerator ag = new ArtGenerator(darknessArray, "B:\\programming\\Ascii-Generator\\out.txt");
-        ag.create();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter image file name: ");
+        String imageFileName = scanner.nextLine();
+        System.out.println("Enter desired output file name: ");
+        String outFileName = scanner.nextLine();
+        System.out.println("Enter scaling percentage: ");
+        double scaling = scanner.nextDouble();
+
+        createArtFromFile(imageFileName, outFileName, scaling);
+    }
+
+    private static void createArtFromFile(String filename, String outFile, double scaling){
+        ImageProcessor processor = new ImageProcessor(filename);
+        processor.scaleImage(scaling, scaling);
+        double[][] darknessArray = processor.getDarknessArray();
+        ArtGenerator generator = new ArtGenerator(darknessArray, outFile);
+        generator.create();
     }
 }
