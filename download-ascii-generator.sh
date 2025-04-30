0726f010fcdd1e6a4575bfc9870abeb42eabb641
Patrick Seute <patrick.seute@cox.net>
2025-04-30 10:26:00 -0400

    update install script to add home bin directory to path if not already included

--- download-ascii-generator.sh
+++ download-ascii-generator.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
+echo "Creating temporary download directory..."
 mkdir ./tmp-ascii-download
 cd tmp-ascii-download/
+echo "Downloading latest release..."
 curl -LO https://github.com/SuperRonJon/Ascii-Generator/releases/latest/download/ascii-generator.zip
+echo "Extracting files..."
 unzip ./ascii-generator.zip
 chmod +x ./ascii-generator.sh
-mkdir -p ~/bin
-mv ./ascii-generator.sh ~/bin/ascii-generator
-mv ./ascii-generator.jar ~/bin/ascii-generator.jar
+mkdir -p $HOME/bin
+mv ./ascii-generator.sh $HOME/bin/ascii-generator
+mv ./ascii-generator.jar $HOME/bin/ascii-generator.jar
 cd ..
 rm -rf tmp-ascii-download/
+PATH=$PATH:$HOME/bin
+echo '[[ ":$PATH:" == *:$HOME/bin:* ]] || PATH="$PATH:$HOME/bin"' >> ~/.bashrc
+source ~/.bashrc
 echo "Finished!"
