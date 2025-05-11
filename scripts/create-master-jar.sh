bc32fcc4cc24bf3dcc78ed38620157175a0c0beb
Patrick Seute <patrick.seute@cox.net>
2025-05-11 09:20:03 -0400

    update jar script to not need to copy jar and better cleanup

--- scripts/create-master-jar.sh
+++ scripts/create-master-jar.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/..
-rm -f target/InputParser.jar target/ascii-generator
-rm -rf target/com/superronjon/inputparse
-rm -f target/ascii-generator-any.deb
+rm -rf target/*
 mkdir -p target/
 javac -cp lib/InputParser.jar -d target/ src/Main.java src/com/superronjon/ascii/AsciiGenerator.java
 cd target/
-cp -u ../lib/InputParser.jar .
-unzip ./InputParser.jar
+unzip ../lib/InputParser.jar -d .
 jar -cfe ascii-generator.jar Main Main.class com/superronjon/ascii/AsciiGenerator.class com/superronjon/inputparse/*.class
+rm -f ../out/ascii-generator.jar
 mkdir -p ../out
-cp -u ascii-generator.jar ../out/ascii-generator.jar
+mv ascii-generator.jar ../out/ascii-generator.jar
