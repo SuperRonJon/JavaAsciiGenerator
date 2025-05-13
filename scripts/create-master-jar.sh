3480623b8d54f64e9c29aabbb5eac9631b5f711e
Patrick Seute <patrick.seute@cox.net>
2025-05-12 21:49:14 -0400

    change java binary version to 8

--- scripts/create-master-jar.sh
+++ scripts/create-master-jar.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/..
 rm -rf target/*
 mkdir -p target/
-javac -cp lib/InputParser.jar -d target/ src/Main.java src/com/superronjon/ascii/AsciiGenerator.java
+javac --release 8 -cp lib/InputParser.jar -d target/ src/Main.java src/com/superronjon/ascii/AsciiGenerator.java
 cd target/
 unzip ../lib/InputParser.jar -d .
 jar -cfe ascii-generator.jar Main Main.class com/superronjon/ascii/AsciiGenerator.class com/superronjon/inputparse/*.class
