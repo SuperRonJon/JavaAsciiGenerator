1849ccdc0a8b9380c7d4ce3e3e126914b3986925
Patrick Seute <patrick.seute@cox.net>
2025-05-16 14:45:51 -0400

    add script to package release

---
+++ scripts/package-release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+VERSION=$1
+OUTPUT_DIR="releases/new-release"
+OUTPUT_NAME="new-release"
+if [ -z "${VERSION}" ]; then
+  echo "No version specified, output to $OUTPUT_DIR"
+else
+  OUTPUT_DIR="releases/ascii-generator_v$VERSION"
+  OUTPUT_NAME="ascii-generator"
+  echo "output to $OUTPUT_DIR"
+fi
+cd $SCRIPT_DIR/../out
+mkdir -p $OUTPUT_DIR
+mv ascii-generator.jar $OUTPUT_DIR/ascii-generator.jar
+cp ../ascii-generator.sh $OUTPUT_DIR/ascii-generator.sh
+cp ../ascii-generator.bat $OUTPUT_DIR/ascii-generator.bat
+cd $OUTPUT_DIR
+tar -czvf $OUTPUT_NAME.tar.gz ./*
