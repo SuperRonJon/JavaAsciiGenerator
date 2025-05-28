781f7bfe82cc048296dd3f2389f9cd2184c5b7ab
Patrick Seute <patrick.seute@cox.net>
2025-05-28 13:00:02 -0400

    add jar creation if it is not created already when packaging

--- scripts/package-release.sh
+++ scripts/package-release.sh
@@ -3,13 +3,19 @@ SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 VERSION=$1
 OUTPUT_DIR="releases/new-release"
 OUTPUT_NAME="ascii-generator"
+cd $SCRIPT_DIR/../out
+
+if [ ! -f "./ascii-generator.jar" ]; then
+  echo "jar not found in out/ directory, running scripts/create-master-jar.sh to create it"
+  ../scripts/create-master-jar.sh
+fi
+
 if [ -z "${VERSION}" ]; then
   echo "No version specified, output to $OUTPUT_DIR"
 else
   OUTPUT_DIR="releases/ascii-generator_v$VERSION"
   echo "output to $OUTPUT_DIR"
 fi
-cd $SCRIPT_DIR/../out
 mkdir -p $OUTPUT_DIR
 mv ascii-generator.jar $OUTPUT_DIR/ascii-generator.jar
 cp ../ascii-generator.sh $OUTPUT_DIR/ascii-generator.sh
